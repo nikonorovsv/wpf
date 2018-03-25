6779d537dfa69aefb2d6c3386e7c4d231f64323a
Sergey Nikonorov <nikonorovsv@gmail.com>
2018-03-25 21:41:04 +0500

    Create HideAdminBar.php

---
+++ app/observers/HideAdminBar.php
@@ -0,0 +1,19 @@
+<?php
+namespace wpf\app\observers;
+
+use \wpf\App;
+use \wpf\app\Observer;
+
+/**
+ * Class HideAdminBar
+ * @package wpf\app\observers
+ */
+class HideAdminBar
+    extends Observer {
+    /**
+     * @param App $app
+     */
+    public function doUpdate( App $app ) {
+        add_filter( 'show_admin_bar', '__return_false' );
+    }
+}
