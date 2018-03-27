115c4efd58007bb093890f3e267651c9d4073984
Sergey Nikonorov <nikonorovsv@gmail.com>
2018-03-27 14:12:39 +0500

    Add Material Icons support

--- helpers/Icon.php
+++ helpers/Icon.php
@@ -1,76 +1,78 @@
 <?php
+
 namespace wpf\helpers;
 
 /**
  * Class Icon
  *
- * @method static $this string fa( string $name, array $options )
+ * @method static $this string fa(string $name, array $options)
  * @package wpf\helpers
  */
-class Icon {
-	/**
-	 * @var string
-	 */
-	public static $tag_name = 'i';
-	/**
-	 * @var array
-	 */
-	public static $prefixes
-		= [
-			'fa' => 'fa fa-',
-			'gi' => 'glyphicon glyphicon-',
-			'ui' => 'icon ',
-			'wp' => 'dashicons dashicons-'
-		];
-	
-	/**
-	 * @param $family
-	 * @param $name
-	 * @param array $options
-	 *
-	 * @return string
-	 */
-	public static function familyIcon( $family, $name, $options = [] ) {
-		$options[ 'prefix' ] = self::getPrefix( $family );
-		
-		return self::i( $name, $options );
-	}
-	
-	/**
-	 * @param $name
-	 *
-	 * @return string
-	 */
-	public static function getPrefix( $name ) {
-		return self::$prefixes[ $name ] ?? '';
-	}
-	
-	/**
-	 * Вывод иконки
-	 *
-	 * @param $name
-	 * @param array $options
-	 *
-	 * @return string
-	 */
-	public static function i( $name, $options = [] ) {
-		$tag    = ArrayHelper::remove( $options, 'tag', self::$tag_name );
-		$prefix = ArrayHelper::remove( $options, 'prefix' );
-		Html::addCssClass( $options, $prefix . $name );
-		
-		return Html::tag( $tag, '', $options );
-	}
-	
-	/**
-	 * @param $name
-	 * @param $arguments
-	 *
-	 * @return mixed
-	 */
-	public static function __callStatic( $name, $arguments ) {
-		array_unshift( $arguments, $name );
-		
-		// TODO
-		return call_user_func_array( '\wpf\helpers\Icon::familyIcon', $arguments );
-	}
+class Icon
+{
+    /**
+     * @var string
+     */
+    public static $tag_name = 'i';
+    /**
+     * @var array
+     */
+    public static $prefixes
+        = [
+            'fa' => 'fa fa-',
+            'gi' => 'glyphicon glyphicon-',
+            'ui' => 'icon ',
+            'wp' => 'dashicons dashicons-',
+            'mi' => 'material-icons'
+        ];
+
+    /**
+     * @param string $family
+     * @param string $name
+     * @param array $options
+     * @return mixed
+     */
+    public static function familyIcon(string $family, string $name, array $options = [])
+    {
+        $class = self::getPrefix($family);
+        // It's Material spike-nail :)
+        if (!self::isMaterial($family)) {
+            $class .= $name;
+            $name = '';
+        }
+        Html::addCssClass($options, $class);
+
+        return Html::tag(self::$tag_name, $name, $options);
+    }
+
+    /**
+     * @param string $name
+     * @return string
+     */
+    private static function getPrefix(string $name): string
+    {
+        return self::$prefixes[$name] ?? '';
+    }
+
+    /**
+     * @param string $family
+     * @return bool
+     */
+    private static function isMaterial(string $family): bool
+    {
+        return ($family == 'mi');
+    }
+
+    /**
+     * @param string $family
+     * @param array $options
+     * @return mixed
+     */
+    public static function __callStatic(string $family, array $options)
+    {
+        // Add as first $family parameter to calling of familyIcon static method.
+        array_unshift($options, $family);
+
+        return call_user_func_array([self::class, 'familyIcon'], $options);
+    }
 }
