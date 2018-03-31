bf4db361286d8ec2d01429e326c0aab13dee9331
Sergey Nikonorov <nikonorovsv@gmail.com>
2018-04-01 03:13:29 +0500

    Create PostStatusDefiner.php

---
+++ app/observers/PostStatusDefiner.php
@@ -0,0 +1,33 @@
+<?php
+namespace wpf\app\observers;
+
+use \wpf\App;
+use \wpf\app\Observer;
+use \wpf\base\ConfigException;
+use \wpf\helpers\ArrayHelper;
+use \wpf\helpers\WP;
+use \InvalidArgumentException;
+
+/**
+ * Class PostStatusDefiner
+ * @package \wpf\app\observers
+ */
+class PostStatusDefiner
+    extends Observer {
+
+    /**
+     * @param App $app
+     * @return bool
+     */
+    public function doUpdate( App $app ) {
+        if ( ! $app->post_statuses ) {
+            return FALSE;
+        } elseif ( ! ArrayHelper::isAssociative( $app->post_statuses ) ) {
+            throw new InvalidArgumentException( __( "Parameter 'post_statuses' must be an object." ) );
+        }
+        // Register all post statuses
+        foreach ( $app->post_statuese as $post_status => $args ) {
+            register_post_status( $post_status, $args );
+        }
+    }
+}
