48e4eb477de86f15c172bee40ca726b0fa9d8f99
Sergey Nikonorov <nikonorovsv@gmail.com>
2018-09-02 02:53:03 +0500

    Create RemoveArchiveTitlePrefix.php

---
+++ app/observers/RemoveArchiveTitlePrefix.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace wpf\app\observers;
+
+use \wpf\app\Observer;
+use \wpf\App;
+
+/**
+ * Class RemoveArchiveTitlePrefix
+ * @package wpf\app\observers
+ */
+class RemoveArchiveTitlePrefix
+    extends Observer {
+    /**
+     * @param App $app
+     *
+     * @return bool
+     */
+    public function doUpdate( App $app ) {
+        add_filter('get_the_archive_title', function( $title ) {
+            return preg_replace('~^[^:]+: ~', '', $title );
+        });
+    }
+}
